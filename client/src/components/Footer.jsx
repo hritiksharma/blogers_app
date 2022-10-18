26379a5cc490673271f52837fb88a6a02da2e8bb
Hritik Sharma <hritik2805@gmail.com>
2022-10-18 16:26:40 +0530

    header and footer integration

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,15 @@
 import React from "react";
-
+import "../style.scss";
+import Logo from "../assets/logo.png";
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <img src={Logo} alt="logo" />
+      <span>
+        Made with ♥️ and <b>React.js</b>.
+      </span>
+    </footer>
+  );
 };
 
 export default Footer;
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,42 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.png";
+import "../style.scss";
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="container">
-        <div className="logo">logo</div>
-        <div className="links">links</div>
+        <div className="logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="links">
+          <Link className="link">
+            <h6>ART</h6>
+          </Link>
+          <Link className="link">
+            <h6>SCIENCE</h6>
+          </Link>
+          <Link className="link">
+            <h6>TECHNOLOGY</h6>
+          </Link>
+          <Link className="link">
+            <h6>CINEMA</h6>
+          </Link>
+          <Link className="link">
+            <h6>DESIGN</h6>
+          </Link>
+          <Link className="link">
+            <h6>FOOD</h6>
+          </Link>
+
+          <span>
+            <h6>Hritik</h6>
+          </span>
+          <span>
+            <h6>LogOut</h6>
+          </span>
+          <span className="write">write</span>
+        </div>
       </div>
     </div>
   );
