a0dcc6484dae4b051c0efa283d451e220fe4536e
Hritik Sharma <hritik2805@gmail.com>
2022-11-02 11:19:25 +0530

    some changes before write

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -3,7 +3,12 @@ import React from "react";
 const Single = () => {
   return (
     <div className="single">
-      <div className="content">c</div>
+      <div className="content">
+        <img
+          src="https://images.pexels.com/photos/5077039/pexels-photo-5077039.jpeg"
+          alt="" />
+        <div></div>
+      </div>
       <div className="menu">m</div>
     </div>
   );
